c85fd7fc93610b97853a688744a8705a2dec5c06
tuan <tuantv.nhnd@gmail.com>
2015-04-12 01:28:03 +0700

    Add solution and data file for challenge 4

---
+++ Set 1/detect_single_character_XOR.py
@@ -0,0 +1,25 @@
+__author__ = 'tuan'
+
+
+import string
+import single_byte_XOR_cipher
+
+def main():
+    with open('4.txt','r+') as f:
+        lines = f.read().splitlines()
+
+    decodedLines = list();
+
+    for line in lines:
+        decodedLines.append(single_byte_XOR_cipher.solve(line))
+    def score(i):
+        return single_byte_XOR_cipher.find_score(decodedLines[i])
+
+    maxI = max(range(len(decodedLines)),key = score)
+
+    print decodedLines[maxI]
+
+
+
+if __name__ == '__main__':
+    main()
