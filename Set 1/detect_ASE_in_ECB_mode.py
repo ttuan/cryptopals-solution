4d74218bcece0ac0e65ed3163f070cba4b8c9850
tuan <tuantv.nhnd@gmail.com>
2015-04-14 16:30:13 +0700

    Add solution for challeng 8

---
+++ Set 1/detect_ASE_in_ECB_mode.py
@@ -0,0 +1,24 @@
+__author__ = 'tuan'
+
+# Be the change you want to see in the world
+
+import collections
+
+
+def find_same_block(line):
+	block_size = 16
+
+	blocks = [line[i: i + block_size] for i in range(0,len(line),block_size)]
+	count = collections.Counter(blocks)
+
+	return count.most_common()[0][1]
+
+
+if __name__ == '__main__':
+	with open('8.txt', 'r') as f:
+		lines = f.readlines()
+	index = 1
+	for line in lines:
+		if find_same_block(line) > 1:
+			print index
+		index += 1
